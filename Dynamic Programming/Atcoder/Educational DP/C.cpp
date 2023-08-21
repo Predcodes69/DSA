475d337d1e9ade98890817b289f16e433b4d87f3
Zaid Alam <predcodes695@gmail.com>
2023-08-21 23:45:07 +0530

    add:more problems tried and AC

---
+++ Dynamic Programming/Atcoder/Educational DP/C.cpp
@@ -0,0 +1,203 @@
+/*
+⡿⠿⠿⠿⠛⡉⠄⣂⢆⢂⠢⠂⡂⡂⡂⡂⡢⢂⢂⠢⢀⢂⢂⢂⢂⢂⠂⡙⢿⣿
+⣿⣶⣤⠌⢀⢨⡸⡸⡸⡰⡨⡂⡂⡂⡂⣂⢂⢂⠢⣈⡂⣂⢂⡢⡪⡢⢐⠀⡂⢻
+⣿⠟⢁⢐⢈⠂⢅⢙⢜⢅⢇⢗⢕⢕⡕⡜⡆⡧⡣⡣⠃⡎⡮⡪⡺⡸⡐⡐⡐⠄
+⠔⠨⢐⠐⠄⠅⠅⡂⠔⡘⡘⡘⠪⡣⡂⡧⡣⢇⠏⡎⠨⢊⢐⠐⠄⢅⢃⢂⠂⢅
+⡖⢀⠂⠅⠅⢅⠑⠄⠅⢂⠢⡈⡂⡂⢌⢐⢐⠀⡊⢰⡀⢂⠢⠡⢑⢐⠐⢄⢑⢐
+⠠⡁⡊⠌⠌⠄⢅⠑⠈⠔⡐⡐⠠⠐⡐⡐⠀⡐⣠⣿⢮⠀⠈⠨⠐⢄⢑⢐⢐⢐
+⠂⡐⠄⠅⠅⢈⠐⠠⢈⢂⠂⠄⠠⠑⣀⠔⣡⠮⠟⢊⣡⢶⡈⠠⢑⢐⢐⠐⡐⠐
+⡇⡐⠡⠡⠁⠀⠐⠀⠀⠀⠀⠡⣶⢾⣳⡖⡀⠀⠀⢀⣀⢠⡀⢀⠅⡂⡂⠅⠈⠄
+⣇⠂⠅⢁⠀⢠⢫⡇⠀⡀⠛⠩⣿⣟⣿⣻⣇⢐⠄⢌⢋⢸⠅⠂⡐⡐⡐⢨⡇⢀
+⣿⠈⢀⡄⡐⠀⢿⢷⡡⢐⢅⢪⣷⢿⣽⣯⡿⣦⣥⡥⣥⢫⡐⡁⡂⠢⡈⠜⠁⣸
+⡟⣠⣾⡧⢐⠁⠌⣿⣺⢽⡯⣿⣽⣟⢷⡻⡝⣷⢷⣻⢾⡽⠂⡂⠊⢀⠄⠠⠀⢾
+⣿⣿⡿⣿⡄⠌⣄⠘⠯⡿⣽⢿⣾⣺⣟⣿⣧⣻⣿⡻⠝⣁⢌⠀⠴⠛⣾⣦⡀⠿
+⡟⣵⣶⣶⡴⢪⣿⢆⡶⣦⣂⢍⣉⠙⠩⢚⢩⠡⠂⠠⢠⣞⣽⣷⢈⠠⠡⢀⠂⣰
+⣧⢻⣿⣿⡟⡸⠀⣞⡟⣿⠞⡈⢅⢐⠀⠄⠃⠄⠂⡅⢄⠛⣳⠵⠃⢥⠈⠄⡚⠿
+⣿⣷⣬⣥⣶⠡⡡⢀⠙⠈⡀⡪⡐⡰⠐⠠⢉⢐⠐⡎⡫⡀⠄⢀⠁⢌⣕⡐⡀⣁
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠟⠛⠛⠛⠿⠟⠟⠻⠛⡋⠋⠉⢻⣿⣿⣿⣿⣿⣿⣿
+⡟⡐⢄⢢⢁⠍⠛⠟⡉⠠⠈⠄⡁⠈⠠⠐⠐⠐⠠⠐⠈⠐⡀⠸⣿⣿⣿⣿⣿⣿
+⡂⢕⢽⢷⡧⣷⣱⠀⢂⠨⢀⠁⠀⠀⠠⠐⠀⠅⠀⠄⠀⠂⢐⠀⢺⣿⣿⣿⣿⣿
+⡇⢜⢜⣿⣾⡛⠄⠂⠄⠐⡀⠀⢄⣢⣶⡶⣿⡾⣾⣾⣶⣦⡐⢈⠀⢿⣿⣿⣿⣿
+⣿⡐⢕⣺⡕⠠⠐⠈⠀⠀⠄⢘⣾⣿⡽⣿⡿⣿⢿⣻⡽⡿⣷⠄⠂⠀⡉⠻⣿⣿
+⣿⡎⠰⢹⠊⠠⠨⠐⠀⠠⠐⣸⢺⣱⣽⣾⣿⣿⣿⣿⣾⣮⡻⣷⠀⠠⠐⠈⣿⣿
+⣿⣿⡈⠢⠈⠠⢁⠂⠠⡁⣜⣾⣿⡿⡿⣯⣷⣿⣾⡿⣾⢿⡿⣿⡐⡐⠨⠀⣿⣿
+⣿⣿⡟⠠⠀⡀⠢⡡⢑⠰⣸⣿⠳⢛⠛⠮⣟⣯⣷⣿⠝⠝⠝⣿⠂⡂⢅⠅⢺⣿
+⣿⣿⡕⢈⠆⠀⢌⠂⡅⠕⡐⡡⣵⣾⢿⣷⣿⣿⢿⣯⣷⣿⣷⣬⠂⠡⠁⡈⣀⢽
+⣿⣿⡟⢄⠕⠀⠐⡑⡄⠡⢸⣺⣽⣿⢿⣻⣗⣟⡿⡺⣿⣽⣾⣟⠄⠀⡅⣼⣿⣿
+⣿⠟⢅⢅⣲⣥⠀⠫⡎⡀⢐⣿⣟⣿⣿⢿⡢⣇⢧⡑⣾⣟⣿⠣⢐⠈⢷⣿⣿⣿
+⣗⡡⣱⣼⣿⣿⣶⡀⠰⠠⡑⢜⢿⣿⣽⣿⣷⡱⡣⣫⣾⡿⠩⠨⠠⠑⢀⣹⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⡇⠈⠈⡂⢅⡣⢯⣛⠷⠿⡿⡽⠟⣑⣤⠀⡁⠅⢁⢸⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⡧⣀⣦⡈⠊⠓⠕⠧⡣⣆⢮⠪⡐⣿⣷⣼⣦⣷⣿⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⡿⢑⢥⣳⣿⢷⣁⠂⠀⠀⢨⡳⠅⡀⢿⣆⡙⠿⣿⣿⣿⣿⣿⣿⠀⠀⠀⠀      */
+/*
+ * 
+ *@author - pred695
+ * 
+ */
+/*弱い も
+خاک سے آیا ہوں، خاک بنوں گا۔*/
+/*-------------------------------------------****************-------------------------------------------------------****************----------------------------------------------------------------------*/
+#include <bits/stdc++.h>
+using namespace std;
+#define vi                          vector<int>
+#define vvi                         vector<vector<int>> 
+#define pb                          push_back
+#define eb                          emplace_back
+#define pii                         pair<int, int>
+#define vp                          vector<pii>
+#define psi                         pair<string, int>
+#define pis                         pair<int, string> 
+#define ppb                         pop_back
+#define pf                          push_front
+#define ppf                         pop_front
+#define all(x)                      x.begin(),x.end()
+#define rep(i, a, b)                for(int i = a; i < b; i++)
+#define repr(i, a, b)               for(int i = a; i >= b; i--)
+#define read(a, n)                  rep(i, 0, n)    cin >> a[i]
+#define readx(a, n, m)             rep(i, 1, n + 1){rep(j, 1, m + 1) cin >> a[i][j];}
+#define print(a, n)                 rep(i, 0, n)    cout << a[i] << " "; cout << endl
+#define printx(a, n, m)            rep(i, 1, n + 1){rep(j, 1, m + 1) cout << a[i][j] << " "; cout << endl;}
+#define endl                        "\n"
+#define ff                          first
+#define ss                          second
+#define yes                         cout << "YES" << endl
+#define no                          cout << "NO" << endl
+#define int                         long long
+#define lld                         long long double
+#define ull                         unsigned long long
+#define si                          set<int>
+#define sc                          set<char>
+#define sti                         stack<int>
+#define stc                         stack<char>
+#define msi                         map<string, int>
+#define mis                         map<int, string>
+#define in                          insert
+#define M                           1000000007
+#define prec(x)                     cout << fixed << setprecision(x)
+#define bpc(x)                      __builtin_popcountll(x)
+const int N = 1e7 + 10;
+
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr << #x <<": "; deb(x); cerr << endl;
+#else
+#define debug(x)
+#endif
+ 
+void deb(int t) {cerr << t;}
+void deb(string t) {cerr << t;}
+void deb(char t) {cerr << t;}
+void deb(double t) {cerr << t;}
+void deb(ull t) {cerr << t;}
+ 
+template <class T, class V> void deb(pair <T, V> p) {cerr << "{"; deb(p.ff); cerr << ","; deb(p.ss); cerr << "}";}
+template <class T> void deb(vector <T> v) {cerr << "[ "; for (T i : v) {deb(i); cerr << " ";} cerr << "]";}
+template <class T> void deb(set <T> v) {cerr << "[ "; for (T i : v) {deb(i); cerr << " ";} cerr << "]";}
+template <class T> void deb(multiset <T> v) {cerr << "[ "; for (T i : v) {deb(i); cerr << " ";} cerr << "]";}
+template <class T, class V> void deb(map <T, V> v) {cerr << "[ "; for (auto i : v) {deb(i); cerr << " ";} cerr << "]";}
+int mul(int a, int b) {
+    return ((a % M) * (b % M)) % M;
+}
+int minv(int a) {
+    return pow(a, M - 2);
+}
+int mdiv(int a, int b) {
+    return mul(a, minv(b));
+}
+int madd(int a, int b) {
+    return (a % M + b % M) % M;
+}
+void fast()
+{
+ios_base::sync_with_stdio(false);
+cin.tie(NULL);
+cout.tie(NULL);
+}
+int ipow(int a, int b)
+{
+   int ans = 1;
+   while(b)
+   {
+    if(b & 1)
+    ans = (ans*a)%M;
+    a = (a*a)%M;
+    b>>=1;
+   }
+   return ans;
+}
+clock_t start;
+void exectime()
+{
+    clock_t end=clock();
+    double time_taken = double(end - start) / double(CLOCKS_PER_SEC);
+    #ifndef ONLINE_JUDGE
+    cout << endl;
+    cout << endl;
+    cout << "Executed in "<< fixed << time_taken << setprecision(10);
+    cout << " secs " << endl;
+    #endif
+}
+vi primes(N, 1);
+void sieve()
+{
+    primes[0] = primes[1] = 0;
+    for (int i = 2; i * i <= N; i++)
+    {
+        if (primes[i])
+            for (int j = i * i; j <= N; j += i)
+                primes[j] = 0;
+    }
+    for (int i = 2; i < N; i++)
+    {
+        if (primes[i] == 1)
+        {
+            primes[i] = i;
+            for (int j = i * i; j <= N; j += i)
+            {
+                if (!primes[j])
+                    primes[j] = i; // storing min primes
+            }
+        }
+    }
+}
+/*----------------------------------------------------------------------MAIN CODE------------------------------------------------------------------------------------------------------------------------*/
+void solve()
+{
+    int n;
+    cin >> n;
+    vvi v(n, vi(3, 0));
+    rep(i, 0, n)
+    {
+        rep(j, 0, 3)
+        {
+            cin >> v[i][j];
+        }
+    }
+    debug(v);
+    vvi dp(n + 10, vi(3, 0));
+    dp[0][0] = v[0][0], dp[0][1] = v[0][1], dp[0][2] = v[0][2];//base case
+    //our state can be dp[i][j] where its value is equal to the maximum points if v[i][j] is chosen according to the rules of the problem
+    //final subproblem being finding the maximum among dp[n - 1][j] where j can range from 0 to 2
+    rep(i, 1, n)
+    {
+       //if for instance, v[i][0] is picked, then v[i - 1][0] cant be picked, the available choices are oly v[i - 1][1] and v[i - 1][2]
+            dp[i][0] = max(v[i][0] + dp[i - 1][1], v[i][0] + dp[i - 1][2]);
+            dp[i][1] = max(v[i][1] + dp[i - 1][2], v[i][1] + dp[i - 1][0]);
+            dp[i][2] = max(v[i][2] + dp[i - 1][1], v[i][2] + dp[i - 1][0]);
+    }
+    cout << max(dp[n - 1][0], max(dp[n - 1][1], dp[n - 1][2])) << endl;
+}
+/*----------------------------------------------------------------------ありがと-------------------------------------------------------------------------------------------------------------------------*/
+signed main()
+{
+fast();
+int t = 1;
+// cin >> t;
+start = clock();
+rep(i, 0, t)
+{
+    // cout << "Case #" << i + 1 << ": ";
+    solve();
+}
+exectime();
+return 0;
+}
